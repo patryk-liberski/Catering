b4e64b581c62d260aa5fa5a122d691ad9482845c
Oleanderek933 <32306815+Oleanderek9@users.noreply.github.com>
2018-05-30 17:03:58 +0200

    Catering

---
+++ resources/javascript/script.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+    
+    /* ----------------------------------- */
+    /* For the sticky navigation */
+    /* ----------------------------------- */
+    
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+    
+    /* ----------------------------------- */
+    /* Scroll on buttons */ 
+    /* ----------------------------------- */
+    
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    /* ----------------------------------- */
+    /* Mobile nav */
+    /* ----------------------------------- */
+    
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon =$('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+        
+    });
+    
+    /* ----------------------------------- */
+    /* Maps */
+    /* ----------------------------------- */
+    
+    var map = new GMaps({
+      div: '.map',
+      lat: 50.0466814,
+      lng: 19.8647904,
+      zoom: 12
+        
+    });
+    
+    map.addMarker({
+        lat: 50.0466814,
+        lng: 19.74,
+        title: 'Kraków',
+        infoWindow: {
+            content: '<p>Centrum Krakowa</p>'
+        }
+    });
+    
+    
+    
+});
